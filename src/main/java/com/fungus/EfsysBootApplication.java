9ffc15303308999e3f23b4c132f7d951fb8d44c1
wdy <wdy123456>
2021-09-03 17:29:43 +0800

    项目初始化

---
+++ src/main/java/com/fungus/EfsysBootApplication.java
@@ -0,0 +1,13 @@
+package com.fungus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfsysBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfsysBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fungus/EfsysBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fungus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfsysBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
